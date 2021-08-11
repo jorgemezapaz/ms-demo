f6504a8482fedc580aadaaf723ed0b20c28905c4
jorgemezapaz <jorge.meza.paz@gmail.com>
2021-08-10 21:34:24 -0400

    first commit

---
+++ src/main/java/com/example/demo/GreetController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+
+    @GetMapping("/greet/{name}")
+    public ResponseEntity<String> greet(@PathVariable("name") String name){
+        return new ResponseEntity<>("Hello "+name, HttpStatus.OK);
+    }
+}
