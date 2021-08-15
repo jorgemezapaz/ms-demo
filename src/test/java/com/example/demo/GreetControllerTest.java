c562391379dfbd1fa47a31bacbbfe1e682548628
jorgemezapaz <jorge.meza.paz@gmail.com>
2021-08-15 13:59:56 -0400

    test controllert

---
+++ src/test/java/com/example/demo/GreetControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+class GreetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+
+    @Test
+    void greet() throws Exception {
+        mockMvc.perform(get("/greet/{name}", "Jorge")
+                        .contentType("application/json"))
+                .andExpect(status().isOk());
+    }
+}
